ab6c3dd4d96ad0d203744c73e417a6d408bca52a
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-09 11:20:20 +0530

    Commit
    Commit

---
+++ src/com/cg/demo/pc/IncrementDecrementDemo.java
@@ -0,0 +1,19 @@
+package com.cg.demo.pc;
+
+public class IncrementDecrementDemo {
+
+	public static void main(String[] args) {
+
+		int i = 10;
+
+		System.out.println(i); // 10 
+		System.out.println(i++); // 10 
+		System.out.println(++i); // 12 
+		System.out.println(i); // 12 
+		System.out.println(i--); // 12 
+		System.out.println(--i); // 10 
+		System.out.println(i); // 10
+
+	}
+
+}
---
+++ src/com/cg/demo/pc/ShortHandDemo.java
@@ -0,0 +1,15 @@
+package com.cg.demo.pc;
+
+public class ShortHandDemo {
+
+	public static void main(String[] args) {
+
+		int i = 10, j = 5;
+//		i += j; 
+		i = i + j;
+		
+		System.out.println(i);
+
+	}
+
+}
