8d55e27a09785642a221e2190dec5d13806ecee9
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-09 10:49:39 +0530

    Create ArithmeticDemo.java

---
+++ src/com/cg/demo/pc/ArithmeticDemo.java
@@ -0,0 +1,26 @@
+package com.cg.demo.pc;
+
+/** 
+ * 
+ * @author Vaman Deshmukh 
+ *
+ */
+
+/**
+ * Arithmetic operators + - / * %
+ */
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+
+		int i = 10, j = 3;
+
+		System.out.println(i + j);
+		System.out.println(i - j);
+		System.out.println(i / j);
+		System.out.println(i * j);
+		System.out.println(i % j);
+
+	}
+
+}
