853894805e1fe7a4cf56a567fa621ea4677bc41c
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-10 11:25:41 +0530

    Update BankCustomer.java

--- src/com/cg/demo/oop/BankCustomer.java
+++ src/com/cg/demo/oop/BankCustomer.java
@@ -2,16 +2,16 @@ package com.cg.demo.oop;
 
 public class BankCustomer {
 
-	// non static (fields and methods) - restricted access  
-	// static (fields and methods) - free access  
+	// non static (fields and methods) - restricted access (object reference)
+	// static (fields and methods) - free access (class reference)
 
 	static int ifsc;
 	int acNo;
 	String name;
 	double balance;
 
-
 	void withdraw() {
+		System.out.println("withdraw");
 	}
 
 	void transfer() {
@@ -20,19 +20,19 @@ public class BankCustomer {
 	void checkBalance() {
 	}
 
-
 	static void deposit() {
 	}
 
 	static void openAccount() {
+		System.out.println("openAccount");
 	}
-	
+
 	public static void main(String[] args) {
 		ifsc = 10;
 //		acNo = 10; 
 		openAccount();
-//		withdraw();
-		
+		BankCustomer obj = new BankCustomer();
+		obj.withdraw(); 
 	}
 
 }
