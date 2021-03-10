b64a71d7f5a75d3523206d4e9bfad2dada4d3157
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-10 12:31:36 +0530

    Commit
    Commit

--- src/com/cg/demo/oop/App.java
+++ src/com/cg/demo/oop/App.java
@@ -4,6 +4,14 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Start");
+		BankCustomer obj = new BankCustomer();
+		obj.checkBalance();
+		
+		obj.balance = 100;
+		
+		BankCustomer.ifsc = "aaa"; 
+
+		BankCustomer.openAccount();
 
 		// 2. create two objects of BankCustomer
 		// and call all the methods and fields appropriately.
--- src/com/cg/demo/oop/BankCustomer.java
+++ src/com/cg/demo/oop/BankCustomer.java
@@ -5,32 +5,52 @@ public class BankCustomer {
 	// non static (fields and methods) - restricted access (object reference)
 	// static (fields and methods) - free access (class reference)
 
-	
+//	access (method/field) 	in context  - reference 
+//			static 			static 		- Class reference - 25 
+//			static 			non static 	- Class reference - 31 
+//			non static 		static 		- object reference - 27 
+//			non static 		non static 	- this reference - 31 
 	
 	static int bankPhone;
-	static String ifsc;
+	static String ifsc; // static member 
 	int acNo;
 	String name;
-	double balance;
+	double balance; // non static member 
 	long phone;
 
-	// 1. complete the following code 
-	
-	void withdraw() {
-		System.out.println("withdraw");
+	// 1. complete the following code
+
+	static void staticMethod() {
+		System.out.println(BankCustomer.ifsc);
+		BankCustomer object = new BankCustomer();
+		System.out.println(object.balance);
 	}
 
-	void transfer() {
+	void nonStaticMethod() {
+		System.out.println(BankCustomer.ifsc);
+		System.out.println(this.balance);
 	}
 
 	void checkBalance() {
+		System.out.println(balance);
+	}
+
+	void withdraw() {
+
+		System.out.println("withdraw");
+
+	}
+
+	void transfer() {
 	}
 
 	static void deposit() {
 	}
 
 	static void openAccount() {
+
 		System.out.println("openAccount");
+
 	}
 
 }
