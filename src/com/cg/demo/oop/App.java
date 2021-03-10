0b85366f31e1f5dcd148c287cce67199e5a4da1a
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-10 11:47:26 +0530

    Update App.java

--- src/com/cg/demo/oop/App.java
+++ src/com/cg/demo/oop/App.java
@@ -5,7 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		// 2. create two objects of BankCustomer and call all the methods appropriately.
+		// 2. create two objects of BankCustomer
+		// and call all the methods and fields appropriately.
 
 	}
 
